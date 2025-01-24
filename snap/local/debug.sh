88c24acce90ed0e041d9cd7c6ca8511db0a88723
ede <edgar.soldin@web.de>
2025-01-24 18:42:43 +0000

    fix:usr: fix snap PATH handling...
    to allow usage custom backend binaries e.g. in /snap/bin unified launcher script moved PATH handling there to correctly handle empty PATH vars
    
    fixes issue #855

--- snap/local/debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo running \'$0\'
-echo PATH=\'$PATH\'
-echo PYTHONPATH=\'$PYTHONPATH\'
-echo python3=\>\'$(which python3)\'
-echo python3.8=\>\'$(which python3.8)\'
-echo gpg=\>\'$(which gpg)\'
--- snap/local/launcher.sh
+++ snap/local/launcher.sh
@@ -3,4 +3,26 @@
   echo Missing env var SNAP.
   exit 1
 }
-"$SNAP"/bin/$(basename "${0%.sh}") "$@"
+
+# append the paths in our snap for our binaries to be used in case they did not exist already
+[ "$DUPL_LAUNCHER" != "DEBUG1" ] && {
+  PATH="${PATH:+$PATH:}/snap/bin:$SNAP/usr/sbin:$SNAP/usr/bin:$SNAP/sbin:$SNAP/bin:/snap/core20/current/usr/bin"
+  export PATH
+}
+
+case "$DUPL_LAUNCHER" in
+  "DEBUG"*)
+    echo running \'$0\'
+    echo PATH=\'$PATH\'
+    echo PYTHONPATH=\'$PYTHONPATH\'
+    echo python3=\>\'$(which python3)\'
+    echo python3.8=\>\'$(which python3.8)\'
+    echo gpg=\>\'$(which gpg)\'
+    # run command if given
+    "$@"
+    exit $?
+    ;;
+esac
+
+# enforce or packaged python with installed modules and readymade librsync
+"$SNAP"/usr/bin/python3.8 "$SNAP"/bin/duplicity "$@"
