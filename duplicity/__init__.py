6e6ecb12ea55bd7fcd6dee40a36c333785b40030
Kenneth Loafman <kenneth@loafman.com>
2025-02-09 12:37:53 -0600

    chg:pkg: Change version for LP.

--- duplicity/__init__.py
+++ duplicity/__init__.py
@@ -21,7 +21,7 @@
 
 import gettext
 
-__version__: str = "3.0.4"
-__reldate__: str = "February 08, 2025"
+__version__: str = "3.0..dev"
+__reldate__: str = "February 09, 2025"
 
 gettext.install("duplicity", names=["ngettext"])
