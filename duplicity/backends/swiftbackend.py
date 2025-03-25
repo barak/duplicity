5a637fd1cd8f8cb5531ff3a26b92fedec89e7647
Kenneth Loafman <kenneth@loafman.com>
2025-03-25 10:03:31 -0500

    chg:pkg: Move import in function to __init__().

--- duplicity/backends/swiftbackend.py
+++ duplicity/backends/swiftbackend.py
@@ -35,6 +35,7 @@ class SwiftBackend(duplicity.backend.Backend):
         duplicity.backend.Backend.__init__(self, parsed_url)
 
         try:
+            from swiftclient.service import SwiftUploadObject
             from swiftclient.service import SwiftService
             from swiftclient import Connection
             from swiftclient import ClientException
@@ -173,8 +174,6 @@ Exception: {str(e)}"""
     def _put(self, source_path, remote_filename):
         lp = os.fsdecode(source_path.name)
         if config.mp_segment_size > 0:
-            from swiftclient.service import SwiftUploadObject
-
             st = os.stat(lp)
             # only upload using Dynamic Large Object if mpvolsize is triggered
             if st.st_size >= config.mp_segment_size:
