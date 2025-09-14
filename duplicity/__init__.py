421e9910aa4b9f965a837e21de7c08d131789bcd
Kenneth Loafman <kenneth@loafman.com>
2025-09-14 07:00:26 -0500

    chg:pkg: Bump version for LP.

--- duplicity/__init__.py
+++ duplicity/__init__.py
@@ -21,7 +21,7 @@
 
 import gettext
 
-__version__: str = "3.0.6.dev7"
-__reldate__: str = "August 02, 2025"
+__version__: str = "3.0.6.dev8"
+__reldate__: str = "September 14, 2025"
 
 gettext.install("duplicity", names=["ngettext"])
