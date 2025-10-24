46030f89e117a2a2c41d41fcab656d9bdebed3d0
Kenneth Loafman <kenneth@loafman.com>
2025-10-24 12:05:05 -0500

    fix:usr: 'duplicity incremental' attempts to fetch remote manifest which is not needed for the current backup
    Fixes #891

--- duplicity/dup_collections.py
+++ duplicity/dup_collections.py
@@ -198,9 +198,6 @@ class BackupSet(object):
         """
         Add local and remote manifest filenames to backup set
         """
-        assert (
-            not self.remote_manifest_name
-        ), f"Cannot set filename of remote manifest to {remote_filename}; already set to {self.remote_manifest_name}."
         self.remote_manifest_name = remote_filename
 
         local_filename_list = config.archive_dir_path.listdir()
