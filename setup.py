3547dc29fca161a227bf5f02b13305364fed31a7
Kenneth Loafman <kenneth@loafman.com>
2025-10-26 11:30:18 -0500

    chg:pkg: Cleanup the build system.
    - Manually apply fixes from @branchv MR 301.
    - Leave ./setup.py build_ext as is.
    - setuptools needs to be in requirements.txt not .dev for cibuildwheel to set up his environment.

--- setup.py
+++ setup.py
@@ -174,15 +174,6 @@ class BuildExtCommand(build_ext):
 
 setup(
     version=Version,
-    packages=[
-        "duplicity",
-        "duplicity.backends",
-        "duplicity.backends.pyrax_identity",
-    ],
-    package_dir={
-        "duplicity": "duplicity",
-        "duplicity.backends": "duplicity/backends",
-    },
     ext_modules=ext_modules,
     data_files=get_data_files(),
     include_package_data=True,
