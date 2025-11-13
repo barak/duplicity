cf48c38fa56ae233fbf67d60d253f6df1db3500f
Kenneth Loafman <kenneth@loafman.com>
2025-11-13 11:02:37 -0600

    chg:pkg: Prep for 3.0.6.1

--- duplicity/__init__.py
+++ duplicity/__init__.py
@@ -21,7 +21,7 @@
 
 import gettext
 
-__version__: str = "3.0.7.dev0"
-__reldate__: str = "November 03, 2025"
+__version__: str = "3.0.6.1"
+__reldate__: str = "November 13, 2025"
 
 gettext.install("duplicity", names=["ngettext"])
